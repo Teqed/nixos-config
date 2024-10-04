68335a5e26c80413bbb0dccb0dfb6f22510b1df6
Timothy Quilling <teqed@shatteredsky.net>
2024-10-04 12:50:56 -0400

    set nix registry and nixPath

--- modules/home-manager/nixcfg.nix
+++ modules/home-manager/nixcfg.nix
@@ -50,14 +50,9 @@ in {
       };
     };
     nix = {
-      # registry = mapAttrs (_: flake: {inherit flake;}) flakeInputs; # Opinionated: make flake registry and nix path match flake inputs
-      # nixPath = mkDefault (mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry); # Add inputs to the system's legacy channels Making legacy nix commands consistent
+      registry = osConfig.nix.registry;
+      nixPath = osConfig.nix.nixPath;
 
-      # registry = osConfig.nix.registry;
-      # nixPath = osConfig.nix.nixPath;
-
-      # registry.nixpkgs.flake = inputs.nixpkgs;
-      # nixPath = mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
       # channel.enable = false; # Opinionated: disable channels # Only available in NixOS
       gc = {
         automatic = mkDefault true;
