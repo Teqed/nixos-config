6b9c83eb43046f03363362efa3fc7dd03b62477d
Timothy Quilling <teqed@shatteredsky.net>
2024-10-05 16:09:23 -0400

    add hardware rule for logitech wireless - required for solaar

--- modules/nixos/desktop/bluetooth.nix
+++ modules/nixos/desktop/bluetooth.nix
@@ -12,5 +12,6 @@ in {
     hardware.bluetooth.enable = lib.mkDefault true; # enables support for Bluetooth
     # hardware.bluetooth.package = pkgs.bluez; # selects the Bluetooth package to use
     hardware.bluetooth.powerOnBoot = lib.mkDefault true; # powers up the default Bluetooth controller on boot
+    hardware.logitech.wireless.enable = lib.mkDefault true; # Linux devices manager for the Logitech Unifying Receiver
   };
 }
