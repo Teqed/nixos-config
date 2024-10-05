cbec1344a6a4cbc6e0d72550c182cffbfdd1fee6
Timothy Quilling <teqed@shatteredsky.net>
2024-10-05 16:13:08 -0400

    cleanup

--- modules/home-manager/packages.nix
+++ modules/home-manager/packages.nix
@@ -17,7 +17,6 @@ in {
     home.packages = with pkgs; [
       lsof # Lsof lists file information about files opened by processes
       kdePackages.tokodon
-      # kdePackages.kdeconnect-kde # Conflicts with kdeconnect wrapped
       kdePackages.wayland-protocols
       wezterm
       (nerdfonts.override {
