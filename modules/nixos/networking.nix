5acd1c251d535568755dc47f9b530d6e86b65ea4
Timothy Quilling <teqed@shatteredsky.net>
2024-10-05 13:08:57 -0400

    remove redundant dhcp option

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -16,7 +16,7 @@ in {
     # still possible to use this option, but it's recommended to use it in conjunction
     # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
     useDHCP = lib.mkDefault true; # Attempt to enable DHCP on all interfaces
-    interfaces.enp1s0.useDHCP = lib.mkDefault true; # Attempt to enable DHCP on the first ethernet interface
+    # interfaces.enp1s0.useDHCP = lib.mkDefault true; # Attempt to enable DHCP on the first ethernet interface # Not needed by default
     wireless.enable = lib.mkDefault false; # Enables wireless support via wpa_supplicant.
     wireless.userControlled.enable = lib.mkDefault true; # Allow normal users to control wpa_supplicant through wpa_gui or wpa_cli. This is useful for laptop users that switch networks a lot and don’t want to depend on a large package such as NetworkManager just to pick nearby access points. When using a declarative network specification you cannot persist any settings via wpa_gui or wpa_cli.
     stevenblack = lib.mkIf cfg.blocking {
