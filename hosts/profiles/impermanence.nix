f050bf7c28543800ae0b1d00088dcfe528dc55c5
Timothy Quilling <teqed@shatteredsky.net>
2024-10-07 14:15:04 -0400

    add imperative notes

--- hosts/profiles/impermanence.nix
+++ hosts/profiles/impermanence.nix
@@ -318,4 +318,5 @@ in {
 # nixos-install --root /mnt/tmpfs --flake .#thoughtful --no-root-passwd
 # --option 'extra-substituters' 'https://chaotic-nyx.cachix.org/' --option extra-trusted-public-keys "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
 # reboot
-
+# Additional imperative notes:
+# - mkdir .local/state/history # else history files can't be written
