73e017790e3b59a61661bd58d7ccb2f28f1c801a
Timothy Quilling <teqed@shatteredsky.net>
2024-10-07 14:14:56 -0400

    adjust mesa packages

--- modules/nixos/desktop/amd.nix
+++ modules/nixos/desktop/amd.nix
@@ -20,6 +20,18 @@ in {
       driversi686Linux.amdvlk # The 32-bit AMDVLK drivers can be used in addition to the Mesa RADV drivers.
     ];
     chaotic.mesa-git.enable = true; # Use the Mesa graphics drivers from the Chaotic-AUR repository.
+    chaotic.mesa-git.extraPackages =  with pkgs; [ 
+      mesa_git.opencl
+      rocmPackages.clr.icd
+      amdvlk 
+      rocm-opencl-icd
+      rocmPackages.rocm-runtime
+      ];
+    chaotic.mesa-git.extraPackages32 = with pkgs.pkgsi686Linux; [
+      pkgs.mesa32_git.opencl
+      driversi686Linux.amdvlk
+      ];
+    chaotic.mesa-git.fallbackSpecialisation = false; # Whether to generate specialisation with stable Mesa.
     systemd.tmpfiles.rules = [
       "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}" # Most software has the HIP libraries hard-coded.
     ];
