ca7824076b03eb59f3d44afd45dcd000646f7392
Timothy Quilling <teqed@shatteredsky.net>
2024-10-13 12:02:54 -0400

    configure media

--- modules/nixos/media.nix
+++ modules/nixos/media.nix
@@ -28,47 +28,47 @@ in {
         openFirewall = true; # 7080
         user = "media"; # defaults to "sabnzbd"
         group = "media"; # defaults to "sabnzbd"
-        configFile = "/var/lib/sabnzbd/sabnzbd.ini";
+        configFile = "/home/media/.local/state/sabnzbd/sabnzbd.ini";
       };
       radarr = {
         enable = true;
         openFirewall = true; # 7878
         user = "media"; # defaults to "radarr"
         group = "media"; # defaults to "radarr"
-        dataDir = "/var/lib/radarr/.config/Radarr";
+        dataDir = "/home/media/.local/state/radarr/.config/Radarr";
       };
       jellyfin = {
         enable = true;
         openFirewall = true; # 8096 # The HTTP/HTTPS ports can be changed in the Web UI, so this option should only be used if they are unchanged, see Port Bindings.
         user = "media"; # defaults to "jellyfin"
         group = "media"; # defaults to "jellyfin"
-        dataDir = "/var/lib/jellyfin";
-        configFile = "/var/lib/jellyfin/config";
-        logDir = "/var/lib/jellyfin"; # /var/log ?
-        cacheDir = "/var/cache/jellyfin";
+        dataDir = "/home/media/.local/state/jellyfin";
+        configDir = "/home/media/.local/state/jellyfin/config";
+        logDir = "/home/media/.local/state/jellyfin/log";
+        cacheDir = "/home/media/.cache/jellyfin";
       };
       tautulli = {
         enable = true;
         openFirewall = true; # 8181
         user = "media"; # defaults to "tautulli"
         group = "media"; # defaults to "tautulli"
-        dataDir = "/var/lib/plexpy";
+        dataDir = "/home/media/.local/state/plexpy";
         port = 8181; # Default
-        configFile = "/var/lib/plexpy/config.ini";
+        configFile = "/home/media/.local/state/plexpy/config.ini";
       };
       readarr = {
         enable = true;
         openFirewall = true; # 8787
         user = "media"; # defaults to "readarr"
         group = "media"; # defaults to "readarr"
-        dataDir = "/var/lib/readarr/.config/Readarr";
+        dataDir = "/home/media/.local/state/readarr/.config/Readarr";
       };
       sonarr = {
         enable = true;
         openFirewall = true; # 8989
         user = "media"; # defaults to "sonarr"
         group = "media"; # defaults to "sonarr"
-        dataDir = "/var/lib/sonarr/.config/NzbDrone";
+        dataDir = "/home/media/.local/state/sonarr/.config/NzbDrone";
       };
       prowlarr = {
         enable = true;
@@ -80,18 +80,18 @@ in {
             version = "1.41.0.8994-f2c27da23";
             src = pkgs.fetchurl {
               url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-              sha256 = lib.fakeSha256;
+              sha256 = "sha256-e1COeawdR0pCF+qQ/xkTn/716iM9kB/fXom5MWHQ0YI=";
             };
           });
         };
       in {
         package = plexpass;
-        enable = true;
+        enable = false;
         openFirewall = true; # 32400
         user = "media"; # defaults to "plex"
         group = "media"; # defaults to "plex"
-        dataDir = "/var/lib/plex/.config/Plex Media Server";
-        configFile = "/var/lib/plex/config.xml";
+        dataDir = "/home/media/.local/state/plex";
+        # dataDir = "/home/media/.local/state/plex/.config/Plex Media Server";
         # accelerationDevices = ["*"];
         # extraPlugins = [
         #   (builtins.path {
