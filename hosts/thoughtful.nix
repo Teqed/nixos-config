5f879a67a3472f1ca689b838063116fcf8df416d
Timothy Quilling <teqed@shatteredsky.net>
2024-10-13 12:58:06 -0400

    cleanup

--- hosts/thoughtful.nix
+++ hosts/thoughtful.nix
@@ -8,14 +8,9 @@
     nixos-hardware.nixosModules.common-cpu-amd
     nixos-hardware.nixosModules.common-gpu-amd
   ];
-  # boot.supportedFilesystems = [ "zfs" ];
-  # nixpkgs.config.allowBroken = true;
-  # boot.zfs.forceImportRoot = false;
-  networking.hostId = "9936699a";
-  teq.nixos.media.enable = true; # Test media hosting
   networking.hostName = "thoughtful"; # /dev/disk/by-partuuid/032b15fe-6dc7-473e-b1a5-d51f4df7ffd6
+  networking.hostId = "9936699a";
   nixpkgs.hostPlatform = "x86_64-linux";
-  # services.xserver.videoDrivers = ["amdgpu"]; # Leave unset for modesetting
   hardware.enableRedistributableFirmware = true;
   hardware.cpu.amd.updateMicrocode = true;
   boot = {
@@ -25,27 +20,7 @@
       efi.canTouchEfiVariables = true;
     };
     initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
-    initrd.kernelModules = ["amdgpu"]; # Make the kernel use the correct driver early
+    initrd.kernelModules = ["amdgpu"];
     kernelModules = ["kvm-amd"];
-    # ------------------------------------------
-    kernelParams = [
-      "video=DP-1:1920x1080@144" # /sys/class/drm/card0-DP-1/status 143.85 Hz
-      "video=DP-2:1920x1080@144" # /sys/class/drm/card0-DP-2/status
-      # "video=DP-3:1920x1080@144" # /sys/class/drm/card0-DP-3/status disconnected
-      "video=HDMI-A-1:1920x1080@60" # /sys/class/drm/card0-HDMI-A-1/status
-      # To figure out the connector names, execute the following command while your monitors are connected:
-      # head /sys/class/drm/*/status
-
-      "boot.shell_on_fail" # Drop to root shell on boot failure
-      "quiet" # Silences boot messages
-      "rd.systemd.show_status=false" # Silences successful systemd messages from the initrd
-      "rd.udev.log_level=3" # Silence systemd version number in initrd
-      "udev.log_priority=3" # Silence systemd version number
-      "boot.shell_on_fail" # If booting fails drop us into a shell where we can investigate
-      "splash" # Show a splash screen
-      "bgrt_disable" # Don't display the OEM logo after loading the ACPI tables
-      "plymouth.use-simpledrm" # Use simple DRM backend for Plymouth
-    ];
   };
-  # xdg.portal.wlr.settings.screencast.output_name = "DP-1";
 }
