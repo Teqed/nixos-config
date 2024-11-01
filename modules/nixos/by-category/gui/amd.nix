cd8cfda63b7ff5e812f848c5fba2a8ec1de5f15d
Timothy Quilling <teqed@shatteredsky.net>
2024-11-01 17:06:22 -0400

    amdvlk

--- modules/nixos/by-category/gui/amd.nix
+++ modules/nixos/by-category/gui/amd.nix
@@ -3,31 +3,55 @@
   config,
   pkgs,
   ...
-}: {
-  imports = [
-    ./amdvlk-348903-fix.nix
-  ];
+}: let 
+  my_amdvlk = pkgs.amdvlk.override {
+    glslang = pkgs.glslang.overrideAttrs (finalAttrs: oldAttrs: {
+      version = "15.0.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "KhronosGroup";
+        repo = "glslang";
+        rev = "refs/tags/${finalAttrs.version}";
+        hash = "sha256-QXNecJ6SDeWpRjzHRTdPJHob1H3q2HZmWuL2zBt2Tlw=";
+      };
+    # Effectively revert the change seen here:
+    # https://github.com/NixOS/nixpkgs/commit/c3948c21ef00fbf8ef02f3c2922e84ba72e8a62b#diff-21ee06ef20d156bedb03da22e28850e6043b04a00706a796e7e427fce44500e9R31
+    cmakeFlags = [];
+    });
+  };
+  my_i686_amdvlk = pkgs.driversi686Linux.amdvlk.override {
+    glslang = pkgs.glslang.overrideAttrs (finalAttrs: oldAttrs: {
+      version = "15.0.0";
+      src = pkgs.fetchFromGitHub {
+        owner = "KhronosGroup";
+        repo = "glslang";
+        rev = "refs/tags/${finalAttrs.version}";
+        hash = "sha256-QXNecJ6SDeWpRjzHRTdPJHob1H3q2HZmWuL2zBt2Tlw=";
+      };
+    cmakeFlags = [];
+    });
+  };
+ in {
   config = lib.mkIf config.teq.nixos.gui.amd {
     hardware.graphics.extraPackages = with pkgs; [
       rocmPackages.clr.icd
-      amdvlk # Modern AMD Graphics Core Next (GCN) GPUs are supported through either radv, which is part of mesa, or the amdvlk package. Adding the amdvlk package to hardware.opengl.extraPackages makes both drivers available for applications and lets them choose.
+      my_amdvlk # Modern AMD Graphics Core Next (GCN) GPUs are supported through either radv, which is part of mesa, or the amdvlk package. Adding the amdvlk package to hardware.opengl.extraPackages makes both drivers available for applications and lets them choose.
       rocm-opencl-icd # Modern AMD Graphics Core Next (GCN) GPUs are supported through the rocm-opencl-icd package.
       rocmPackages.rocm-runtime # OpenCL Image support is provided through the non-free rocm-runtime package.
     ];
     hardware.graphics.extraPackages32 = with pkgs; [
-      driversi686Linux.amdvlk # The 32-bit AMDVLK drivers can be used in addition to the Mesa RADV drivers.
+      my_i686_amdvlk # The 32-bit AMDVLK drivers can be used in addition to the Mesa RADV drivers.
     ];
     chaotic.mesa-git.enable = true; # Use the Mesa graphics drivers from the Chaotic-AUR repository.
     chaotic.mesa-git.extraPackages = with pkgs; [
       mesa_git.opencl
       rocmPackages.clr.icd
-      amdvlk
+      my_amdvlk
       rocm-opencl-icd
       rocmPackages.rocm-runtime
     ];
     chaotic.mesa-git.extraPackages32 = with pkgs.pkgsi686Linux; [
       pkgs.mesa32_git.opencl
-      driversi686Linux.amdvlk
+      my_i686_amdvlk
     ];
     chaotic.mesa-git.fallbackSpecialisation = false; # Whether to generate specialisation with stable Mesa.
     systemd.tmpfiles.rules = [
