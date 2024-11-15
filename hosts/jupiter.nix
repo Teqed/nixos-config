ea0c1d8714782a896e63e6a11e375dcc0e8ff01e
Timothy Quilling <teqed@shatteredsky.net>
2024-11-14 23:40:39 -0500

    downgrade foundryvtt to 11

--- hosts/jupiter.nix
+++ hosts/jupiter.nix
@@ -61,7 +61,7 @@
         proxyPort = 443;
         proxySSL = true;
         upnp = false;
-        package = inputs.foundryvtt.packages.${pkgs.system}.foundryvtt_12;
+        package = inputs.foundryvtt.packages.${pkgs.system}.foundryvtt_11;
       };
     };
   };
