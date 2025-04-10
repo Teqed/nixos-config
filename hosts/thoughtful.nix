c256263a5180c7289afd521194b19e2602925926
Timothy Quilling <teqed@shatteredsky.net>
2025-04-10 15:44:34 -0400

    increase boot config limit to 14

--- hosts/thoughtful.nix
+++ hosts/thoughtful.nix
@@ -14,7 +14,7 @@
   boot = {
     loader = {
       systemd-boot.enable = true;
-      systemd-boot.configurationLimit = 7;
+      systemd-boot.configurationLimit = 14;
       efi.canTouchEfiVariables = true;
     };
     initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
