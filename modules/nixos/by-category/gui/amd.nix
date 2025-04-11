ac2d1990e869d73e4b18e14d36b4387adfce14d6
Timothy Quilling <teqed@shatteredsky.net>
2025-04-10 21:30:27 -0400

    disable mesa-git

--- modules/nixos/by-category/gui/amd.nix
+++ modules/nixos/by-category/gui/amd.nix
@@ -10,25 +10,25 @@
       radeontop # For monitoring AMD GPU usage
       vulkan-tools # For Vulkan tools like 'vkcube' and 'vulkaninfo'
     ];
-    chaotic.mesa-git.enable = true; # Use the Mesa graphics drivers from the Chaotic-AUR repository.
-    chaotic.mesa-git.extraPackages = with pkgs; [
-      mesa_git
-      mesa_git.opencl
-      rocmPackages.clr.icd
-      amdvlk
-      # rocm-opencl-icd
-      rocmPackages.rocm-runtime
-      intel-media-driver
-      vaapiIntel
-    ];
-    chaotic.mesa-git.extraPackages32 = with pkgs.pkgsi686Linux; [
-      mesa32_git
-      pkgs.mesa32_git.opencl
-      driversi686Linux.amdvlk
-      intel-media-driver
-      vaapiIntel
-    ];
-    chaotic.mesa-git.fallbackSpecialisation = true; # Whether to generate specialisation with stable Mesa.
+    # chaotic.mesa-git.enable = true; # Use the Mesa graphics drivers from the Chaotic-AUR repository.
+    # chaotic.mesa-git.extraPackages = with pkgs; [
+    #   mesa_git
+    #   mesa_git.opencl
+    #   rocmPackages.clr.icd
+    #   amdvlk
+    #   # rocm-opencl-icd
+    #   rocmPackages.rocm-runtime
+    #   intel-media-driver
+    #   vaapiIntel
+    # ];
+    # chaotic.mesa-git.extraPackages32 = with pkgs.pkgsi686Linux; [
+    #   mesa32_git
+    #   pkgs.mesa32_git.opencl
+    #   driversi686Linux.amdvlk
+    #   intel-media-driver
+    #   vaapiIntel
+    # ];
+    # chaotic.mesa-git.fallbackSpecialisation = true; # Whether to generate specialisation with stable Mesa.
     systemd.tmpfiles.rules = [
       "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}" # Most software has the HIP libraries hard-coded.
     ];
