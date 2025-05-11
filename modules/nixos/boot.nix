818b19ce118287f6d52761e849bb445d3c972e78
Timothy Quilling <teqed@shatteredsky.net>
2025-05-11 15:58:14 -0400

    adjust kernel paramels, kmscon, plymouth, verbosity -- needs attention

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -23,30 +23,29 @@ in {
         #   ssh.enable = mkDefault false; # Optional SSH in the initrd
         #   ssh.ignoreEmptyHostKeys = mkDefault true;
         # };
-        verbose = mkDefault false; # Remove extra NixOS logging from the initrd
+#        verbose = mkDefault false; # Remove extra NixOS logging from the initrd
       };
       plymouth = {
         enable = mkDefault true;
-        theme = mkDefault "hud_3";
-        themePackages = mkDefault [
-          (pkgs.adi1090x-plymouth-themes.override {selected_themes = ["hud_3"];})
-        ];
+#        theme = mkDefault "hud_3";
+#        themePackages = mkDefault [
+#          (pkgs.adi1090x-plymouth-themes.override {selected_themes = ["hud_3"];})
+#        ];
       };
       kernelParams = [
-        "boot.shell_on_fail" # Drop to root shell on boot failure
-        "quiet" # Silences boot messages
-        "rd.systemd.show_status=false" # Silences successful systemd messages from the initrd
-        "rd.udev.log_level=3" # Silence systemd version number in initrd
-        "udev.log_priority=3" # Silence systemd version number
-        "boot.shell_on_fail" # If booting fails drop us into a shell where we can investigate
-        "splash" # Show a splash screen
-        "bgrt_disable" # Don't display the OEM logo after loading the ACPI tables
-        "plymouth.use-simpledrm" # Use simple DRM backend for Plymouth
+#        "quiet" # Silences boot messages
+#        "rd.systemd.show_status=false" # Silences successful systemd messages from the initrd
+#        "rd.udev.log_level=3" # Silence systemd version number in initrd
+#        "udev.log_priority=3" # Silence systemd version number
+#        "boot.shell_on_fail" # If booting fails drop us into a shell where we can investigate
+#        "splash" # Show a splash screen
+#        "bgrt_disable" # Don't display the OEM logo after loading the ACPI tables
+#        "plymouth.use-simpledrm" # Use simple DRM backend for Plymouth
       ];
-      consoleLogLevel = mkDefault 3; # Silence dmesg
+#      consoleLogLevel = mkDefault 3; # Silence dmesg
     };
     services.kmscon = {
-      enable = mkDefault true; # Use kmscon as the virtual console instead of gettys.
+#      enable = mkDefault true; # Use kmscon as the virtual console instead of gettys.
       fonts = [
         {
           name = "Noto Sans Mono";
