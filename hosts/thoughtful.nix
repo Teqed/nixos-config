cc7d0e9799d12b681c1f3cd8868d56bc9e00766a
Timothy Quilling <teqed@shatteredsky.net>
2025-05-11 15:59:46 -0400

    adjust kernel params; set unreasonable number of configurations

--- hosts/thoughtful.nix
+++ hosts/thoughtful.nix
@@ -14,28 +14,27 @@
   boot = {
     loader = {
       systemd-boot.enable = true;
-      systemd-boot.configurationLimit = 32;
+      systemd-boot.configurationLimit = 30;
       efi.canTouchEfiVariables = true;
     };
     initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
     initrd.kernelModules = ["amdgpu"];
     kernelModules = ["kvm-amd"];
     kernelParams = [
-      "video=DP-1:1920x1080@144" # /sys/class/drm/card0-DP-1/status 143.85 Hz
-      "video=DP-2:1920x1080@144" # /sys/class/drm/card0-DP-2/status
-      # "video=DP-3:1920x1080@144" # /sys/class/drm/card0-DP-3/status disconnected
-      "video=HDMI-A-1:1920x1080@60" # /sys/class/drm/card0-HDMI-A-1/status
+      # "video=DP-1:1920x1080@144" # /sys/class/drm/card0-DP-1/status 143.85 Hz
+      # "video=DP-2:1920x1080@144" # /sys/class/drm/card0-DP-2/status
+      # # "video=DP-3:1920x1080@144" # /sys/class/drm/card0-DP-3/status disconnected
+      # "video=HDMI-A-1:1920x1080@60" # /sys/class/drm/card0-HDMI-A-1/status
       # To figure out the connector names, execute the following command while your monitors are connected:
       # head /sys/class/drm/*/status
-      "boot.shell_on_fail" # Drop to root shell on boot failure
-      "quiet" # Silences boot messages
-      "rd.systemd.show_status=false" # Silences successful systemd messages from the initrd
-      "rd.udev.log_level=3" # Silence systemd version number in initrd
-      "udev.log_priority=3" # Silence systemd version number
-      "boot.shell_on_fail" # If booting fails drop us into a shell where we can investigate
-      "splash" # Show a splash screen
-      "bgrt_disable" # Don't display the OEM logo after loading the ACPI tables
-      "plymouth.use-simpledrm" # Use simple DRM backend for Plymouth
+#      "quiet" # Silences boot messages
+#      "rd.systemd.show_status=false" # Silences successful systemd messages from the initrd
+#      "rd.udev.log_level=3" # Silence systemd version number in initrd
+#      "udev.log_priority=3" # Silence systemd version number
+#      "boot.shell_on_fail" # If booting fails drop us into a shell where we can investigate
+#      "splash" # Show a splash screen
+#      "bgrt_disable" # Don't display the OEM logo after loading the ACPI tables
+#      "plymouth.use-simpledrm" # Use simple DRM backend for Plymouth
     ];
   };
   teq.nixos = {
