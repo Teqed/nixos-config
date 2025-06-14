c6b7815fc5c4773a01a6283704e072dca7607d46
Timothy Quilling <teqed@shatteredsky.net>
2025-06-13 20:47:38 -0400

    add postgresql conf

--- modules/nixos/by-category/development.nix
+++ modules/nixos/by-category/development.nix
@@ -5,8 +5,37 @@
   ...
 }: {
   config = lib.mkIf config.teq.nixos.enable {
-    # services = {
-    # };
+    services = {
+      postgresql = {
+        enable = false;
+        ensureDatabases = [ "pds" ];
+          identMap = ''
+              # ArbitraryMapName systemUser DBUser
+              superuser_map      root      postgres
+              superuser_map      teq       postgres
+              superuser_map      postgres  postgres
+              # Let other names login as themselves
+              superuser_map      /^(.*)$   \1
+          '';
+          authentication = pkgs.lib.mkOverride 10 ''
+            #type database  DBuser  auth-method optional_ident_map
+            local all       teq     peer        map=superuser_map
+            local all       postgres peer        map=superuser_map
+            local sameuser  all     peer        map=superuser_map
+          '';
+        package = pkgs.postgresql_16;
+        settings = {
+          # listen_addresses = "*";
+          # max_connections = 100;
+          # shared_buffers = "128MB";
+          # effective_cache_size = "256MB";
+          # work_mem = "4MB";
+          # maintenance_work_mem = "64MB";
+          # max_wal_size = "1GB";
+          # min_wal_size = "80MB";
+        };
+      };
+    };
     programs = {
       ### compilers
       java = {
