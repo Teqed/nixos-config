71c77ec784f677bb081aa9368bf5917611a06bf6
Timothy Quilling <teqed@shatteredsky.net>
2025-08-02 19:52:38 -0400

    add vm pkgs

--- hosts/thoughtful.nix
+++ hosts/thoughtful.nix
@@ -43,6 +43,15 @@
 #      "plymouth.use-simpledrm" # Use simple DRM backend for Plymouth
     ];
   };
+  # VM
+  programs.dconf.enable = true;
+  users.users.gcis.extraGroups = [ "libvirtd" ];
+  users.users.gcis.group = "gcis";
+  users.groups.gcis = {};
+  users.users.gcis.isSystemUser = true;
+  environment.systemPackages = with pkgs; [ virt-manager virt-viewer spice spice-gtk spice-protocol win-virtio win-spice adwaita-icon-theme ];
+  virtualisation = { libvirtd = { enable = true; qemu = { swtpm.enable = true; ovmf.enable = true; ovmf.packages = [ pkgs.OVMFFull.fd ]; }; }; spiceUSBRedirection.enable = true; }; services.spice-vdagentd.enable = true;
+  # /VM
   teq.nixos = {
     media = false;
     cachyos = true;
