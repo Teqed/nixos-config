c47e9e6a04312e7d1cd9317d41c1c3d6f9f849c7
Timothy Quilling <teqed@shatteredsky.net>
2025-08-22 15:49:43 -0400

    swap module imports

--- hosts/bubblegum.nix
+++ hosts/bubblegum.nix
@@ -5,15 +5,20 @@
 {
   imports = [
     ./profiles/common.nix
-    nixos-hardware.nixosModules.common-pc
-    nixos-hardware.nixosModules.common-pc-ssd
-    nixos-hardware.nixosModules.common-pc-laptop
     nixos-hardware.nixosModules.framework-12-13th-gen-intel
   ];
   networking.hostName = "bubblegum";
   # networking.hostId = "9936699a"; # head -c 8 /etc/machine-id
   nixpkgs = {
     buildPlatform = "x86_64-linux";
+  };
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      systemd-boot.configurationLimit = 12;
+      efi.canTouchEfiVariables = true;
+    };
+    initrd.kernelModules = [ "pinctrl_tigerlake" ];
   };
   home-manager.users.teq.teq.home-manager.gui = true;
   teq.nixos = {
