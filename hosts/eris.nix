e97be808a44f51e63094d24048bad859639ce026
Timothy Quilling <teqed@shatteredsky.net>
2026-05-02 17:59:01 -0400

    fix eris; ignore build outputs

--- hosts/eris.nix
+++ hosts/eris.nix
@@ -1,9 +1,11 @@
-{...}: {
+{lib, ...}: {
   imports = [
     ./profiles/usb.nix
     ./profiles/common.nix
   ];
   config = {
     networking.hostName = "eris"; # U+2BF0 ⯰ ERIS FORM ONE
+    # Break mkDefault tie with installation-device.nix; installer needs root login.
+    services.openssh.settings.PermitRootLogin = lib.mkForce "yes";
   };
 }
